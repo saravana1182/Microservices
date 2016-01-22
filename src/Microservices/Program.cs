88d207d952337ba7fc194736a811506688bf9694
saravana1182 <saravanadhinakaran@gmail.com>
2016-01-22 09:16:24 +0530

    MicroSerivices PCO

---
+++ src/Microservices/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Microservices
+{
+    public class Program
+    {
+        public async void Main(string[] args)
+        {
+            var length = await Method1();
+            Console.WriteLine(length);
+            Console.WriteLine("ready");
+            
+            Console.ReadLine();
+        }
+
+        public async Task<int> Method1()
+        {
+            HttpClient client = new HttpClient();
+
+            Task<string> getStringTask = client.GetStringAsync("http://google.com") ;
+
+
+            string urlContents = await getStringTask;
+
+            // The return statement specifies an integer result.
+            // Any methods that are awaiting AccessTheWebAsync retrieve the length value.
+            return urlContents.Length;
+
+        }
+    }
+}
